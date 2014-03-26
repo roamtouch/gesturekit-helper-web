cf2ae1dca8f900e4ac7e2293a74bf249a8a8bd99
Guille Paz <guille87paz@gmail.com>
2014-03-25 21:25:35 -0300

    #1 It should draw a circle on the initial point.

--- helper.js
+++ helper.js
@@ -7,7 +7,7 @@
 
     var doc = window.document,
 
-        helperImage = 'https://i.cloudup.com/xGIfnHRDjw-1200x1200.png', // https://i.cloudup.com/6xhE1qpu0r.gif,
+        helperImage = 'https://i.cloudup.com/jAmu8s95gF-3000x3000.png',
 
         viewport = doc.documentElement,
 
@@ -76,6 +76,8 @@
 
         this._motion = false;
 
+        this._move = false;
+
         this.currentOffset = {
             'x': 2,
             'y': 60
@@ -101,7 +103,7 @@
      */
     Helper.prototype._createDisplay = function () {
         var styles = [
-            'background-color: #626366;',
+            'background-color: #999999;',
             'background-image: url("' + helperImage + '");',
             'background-size: cover;',
             'border-radius: 10px;',
@@ -186,6 +188,8 @@
                 that.currentOffset.x = x;
                 that.currentOffset.y = y;
 
+                that._move = true;
+
                 return;
             }
         });
@@ -193,11 +197,17 @@
         gesturekit.on('pointerend', function (eve) {
             if (eve.target === that.display) {
 
+                if (!that._move) {
+                    console.log('Show Helper');
+                }
+
                 if (that._options.snap) {
                     that._snap();
                 }
 
                 gesturekit.enable();
+
+                that._move = false;
             }
         });
 
@@ -260,19 +270,23 @@
             point;
 
         for (i; i < len; i += 1) {
-
             touch = touches[i];
             id = touch.identifier;
             point = this._scalePoints(touch.pageX, touch.pageY);
 
+            ctx.beginPath();
+
             if (this.lastPoint[id]) {
-                ctx.beginPath();
                 ctx.moveTo(this.lastPoint[id].x, this.lastPoint[id].y);
                 ctx.lineTo(point.x, point.y);
                 ctx.strokeStyle = '#FFF';
-                ctx.lineWidth = 2;
+                ctx.lineWidth = 3;
                 ctx.lineCap = 'round';
                 ctx.stroke();
+            } else {
+                ctx.arc(point.x, point.y, 3.5, 0, 2*Math.PI);
+                ctx.fillStyle = '#FFF';
+                ctx.fill();
             }
 
             this.lastPoint[id] = {x: point.x, y: point.y};
